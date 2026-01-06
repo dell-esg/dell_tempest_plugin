9cc0adc24579b03f8c149f3fb8181a04e274577f
Prasant-Dell <51162948+Prasant-Dell@users.noreply.github.com>
2026-01-06 11:19:25 +0530

    Prasant/ecs03 f 194 (#8)
    * Adding migrate volume test case for Powerstore

--- dell_tempest_plugin/services/failover_client.py
+++ dell_tempest_plugin/services/failover_client.py
@@ -1,15 +1,22 @@
 import json
 from tempest.lib.services.volume.v3.services_client import ServicesClient
+from tempest.lib import exceptions as lib_exc
 
 class DellFailoverClient(ServicesClient):
 
-    def failover_host(self, host_name, backend_id=None):
-        post_body = {
-            'failover_host': {
-                'backend_id': backend_id
-            }
-        }
-        url = 'os-volume-hosts/%s/action' % host_name
+    def failover_host(self, host: str, backend_id: str | None = None):
+        url = 'os-services/failover-host'      # relative v3 path
         headers = {'Content-Type': 'application/json'}
-        resp, body = self.post(url, headers=headers, body=json.dumps(post_body))
-        return body
+        body = {'host': host}
+        if backend_id is not None:
+            body['backend_id'] = backend_id
+
+        # POST /v3/{project_id}/os-services/failover-host
+        # BaseClient will prefix with /v3/{project_id}
+        resp, resp_body = self.post(url, headers=headers, body=json.dumps(body))
+        # Expected: 202 Accepted (operation is async)
+        if not (200 <= resp.status <= 299):
+            raise lib_exc.TempestException(
+                f"Failover-host returned HTTP {resp.status}: {resp_body!r}"
+            )
+        return resp, resp_body
